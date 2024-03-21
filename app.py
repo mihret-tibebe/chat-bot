06cdc83ef843e178ca0ea5746f21c714b1338fa3
Mihret Tibebe <88349462+mihret-tibebe@users.noreply.github.com>
2024-03-21 08:15:36 +0300

    Update app.py

--- app.py
+++ app.py
@@ -17,9 +17,9 @@ with app.app_context():
 def index():
     return render_template('index.html')
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
+# @app.route('/about')
+# def about():
+#     return render_template('about.html')
 
 @app.route('/chatbot')
 def chatbot():
