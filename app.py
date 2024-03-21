62be964c8959adbdeb15d12ee5b06deeaa0b496a
Mihret Tibebe <88349462+mihret-tibebe@users.noreply.github.com>
2024-03-21 10:57:34 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, render_template, url_for
+from flask import Flask, request, jsonify, render_template, url_for, redirect 
 from inference_engine import run_expert_system
 from config import HOST, PORT, SQLALCHEMY_DATABASE_URI, SQLALCHEMY_TRACK_MODIFICATIONS
 # from models import db, Prediction, Syndrom
